7efab2a1f305cc691c1959ee2b8eae043d22c5b1
Matthew Wong <matthew@thewongcode.net>
2019-11-29 02:20:26 -0700

    Added nodemon to update changes, server working

--- app.js
+++ app.js
@@ -8,9 +8,8 @@ const indexRouter = require('./routes/index');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// USE THIS TO SET TO DEVELOPMENT OR RELEASE
+app.set('env', 'development')
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,7 +18,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -27,10 +25,10 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
